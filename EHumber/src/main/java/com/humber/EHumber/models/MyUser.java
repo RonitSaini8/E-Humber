c01773145e0cc2bb55ff6b0b4fc054cb4b15bd43
RonitSaini8 <146130509+RonitSaini8@users.noreply.github.com>
2025-04-09 12:57:12 -0400

    Added user model

---
+++ EHumber/src/main/java/com/humber/EHumber/models/MyUser.java
@@ -0,0 +1,25 @@
+package com.humber.EHumber.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class MyUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
